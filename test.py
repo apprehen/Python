8fe8f34ec7e98793540f6008a987f65a5abdf862
apprehen <3514392356@qq.com>
2022-11-02 21:47:26 +0800

    update

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-try:
-  print('try...')
-  r =  10/0
-  print('result:',r)
-except ZeroDivisionError as e:
-  print('except:',e)
-finally:
-  print('finally...')
-print('END')
+import psycopg2
+
+conn = psycopg2.connect(database="test_db", user="postgres", password="Apprehen", host="127.0.0.1", port="5432")
+print("PostgreSQL连接成功")
+
+cur = conn.cursor()
+cur.execute('''CREATE TABLE COMPANY
+        (ID INT PRIMARY KEY     NOT NULL,
+        NAME           TEXT    NOT NULL,
+        AGE            INT     NOT NULL,
+        ADDRESS        CHAR(50),
+        SALARY         REAL);''')
+print ("Table created successfully") 
+conn.commit()
+conn.close()
